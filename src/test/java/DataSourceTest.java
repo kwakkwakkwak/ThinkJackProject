2c8f5ebf8755421ee5e495a25770f54a441c0555
kwak <kwakkj112@naver.com>
2017-12-02 16:04:35 +0900

    first commit

---
+++ src/test/java/DataSourceTest.java
@@ -0,0 +1,28 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.inject.Inject;
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/**/applicationContext.xml"})
+public class DataSourceTest {
+
+    @Inject
+    private DataSource ds;
+
+    @Test
+    public void DataSourceTest() throws Exception{
+        try (Connection con = ds.getConnection()) {
+
+            System.out.println(con);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
